71d6c9c712122c841988b998ee8828ad2eb7fd2f
Alex Izvorski <aizvorski@gmail.com>
2015-03-11 01:42:56 -0700

    Fix crash when images are identical

--- psnr.py
+++ psnr.py
@@ -20,6 +20,8 @@ import numpy
 import math
 
 def psnr(img1, img2):
-	mse = numpy.mean( (img1 - img2) ** 2 )
-	PIXEL_MAX = 255.0
-	return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+    mse = numpy.mean( (img1 - img2) ** 2 )
+    if mse == 0:
+        return 100
+    PIXEL_MAX = 255.0
+    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
